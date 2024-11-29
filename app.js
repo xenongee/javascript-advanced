8331a450da35173254dce81c2fadaa9ebc6f247a
xenongee <rublgleb@gmail.com>
2024-11-30 00:51:45 +0300

    Operations with dates

--- app.js
+++ app.js
@@ -3,7 +3,6 @@
 /*
 	Creating dates
     Создание дат
-*/
 
 const now = new Date();
 const day = now.getDate();
@@ -24,3 +23,29 @@ console.log(new Date(1996, 5, 1258));
 
 console.log(Date.now()); // unux datestamp
 console.log(new Date(now.setFullYear(2033))); // change year
+*/
+
+/*
+	Operations with dates
+	Операции с датами
+*/
+
+const date1 = new Date(2024, 10, 30);
+const date2 = new Date(2024, 11, 25);
+
+console.log(Number(date1), Number(date2));
+console.log(date2 - date1);
+
+function getDaysBetweenDates(datefirst, datesecond) {
+	return (date2 - date1) / (1000 * 60 * 60 * 24);
+}
+
+console.log(getDaysBetweenDates(date1, date2));
+
+console.log(date1);
+console.log(+date1); 
+console.log(date.setTime(date1.getTime() + 1000 * 60 * 60 * 24)); // add 1 day
+console.log(new Date(date2.setDate(date.getDate() + 1))); // add 1 day
+
+	
+
