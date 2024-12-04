b0bf86f01bc0afa522504111f6142deef0ecebd0
xenongee <rublgleb@gmail.com>
2024-12-04 18:07:18 +0300

    Prototype pt2

--- app.js
+++ app.js
@@ -33,7 +33,40 @@ const journal = function(title, employee) {
 	this.title = title;
 	this.employee = employee;
 	this.createdAt = new Intl.DateTimeFormat('ru-RU').format(new Date());
+    this.readed = false;
+}
+
+journal.prototype.read = function() {
+    this.readed = true;
 }
 
 const securityJournal = new journal('Security', 'John Doe', '2020-01-01');
 const machineElementsJournal = new journal('Machine Elements', 'Vasya Pupkin', '2020-01-01');
+
+console.log(securityJournal.title, securityJournal, machineElementsJournal.title, machineElementsJournal);
+
+securityJournal.read();
+machineElementsJournal.read();
+
+console.log(securityJournal.title, securityJournal);
+console.log(machineElementsJournal.title, machineElementsJournal);
+// biome-ignore lint/suspicious/noPrototypeBuiltins: <explanation>
+console.log('hasOwnProperty(editedAt):', machineElementsJournal.hasOwnProperty('editedAt')); // false
+// biome-ignore lint/suspicious/noPrototypeBuiltins: <explanation>
+console.log('hasOwnProperty(employee):', machineElementsJournal.hasOwnProperty('employee')); // true
+
+console.log(securityJournal.__proto__); // {read: f}
+
+console.log('proto?', machineElementsJournal.__proto__ === journal.prototype); // true
+// biome-ignore lint/suspicious/noPrototypeBuiltins: <explanation>
+console.log('proto?',journal.prototype.isPrototypeOf(securityJournal)); // true
+// biome-ignore lint/suspicious/noPrototypeBuiltins: <explanation>
+console.log('proto?',journal.prototype.isPrototypeOf(journal)); // false
+
+// some example
+// bad practice
+Array.prototype.firstElem = () => {
+    console.log('first element: ', [0]);
+};
+
+console.log([1,2,3].firstElem());
